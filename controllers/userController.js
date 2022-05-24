c06c5b1b80ab07e5aef5f4a1857e3230e1309fe8
jose heron samperio leon <joseheronsamperio@gmail.com>
2022-05-24 12:05:03 -0500

    returns userID at mapperContributions and last update on mapper details

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,7 +18,9 @@ module.exports.List = (request, response) => {
 
   connection.query(sql, (error, rows) => {
     if (error) response.send(error);
+
     let obj = [{}];
+
     for (let x = 0; x < rows.length; x++) {
       obj[x] = {
         name: {
@@ -42,7 +44,7 @@ module.exports.List = (request, response) => {
 module.exports.Table = (request, response) => {
   var userId = request.params.id;
 
-  var sql = `select distinct a.name ,r.UPDATED_AT,ADDRESS,a.country_name,a.CITY
+  var sql = `select distinct u.ID, a.name, r.UPDATED_AT, ADDRESS, a.country_name, a.CITY
   from ams_dashboard_users u join ams_dashboard_accommodations a on u.UID=a.USER_UID
   join ams_dashboard_replies r on r.ACCOMMODATION_UID=a.accommodation_uid
   where u.ID=${userId} order by(r.UPDATED_AT)`;
@@ -57,10 +59,12 @@ module.exports.Table = (request, response) => {
 
   connection.query(sql, (error, rows) => {
     if (error) response.send(error);
+
     let obj = [{}];
 
     for (let x = 0; x < rows.length; x++) {
       obj[x] = {
+        id: rows[x].ID,
         placeName: rows[x].name,
         city: `${rows[x].CITY}, ${rows[x].country_name}`,
         progress: 0, //implementar progreso
@@ -73,26 +77,40 @@ module.exports.Table = (request, response) => {
 //MAPPER DETAILS **implementar params
 module.exports.Details = (request, response) => {
   var userId = request.params.id;
-  var sql = `select a.name, r.UPDATED_AT,INQUIRY_ID,ADDRESS,a.country_name,a.CITY
+  var sql = `
+  select a.name, r.CREATED_AT,INQUIRY_ID,ADDRESS,a.country_name,a.CITY
   from ams_dashboard_users u join ams_dashboard_accommodations a on u.UID=a.USER_UID
   join ams_dashboard_replies r on r.ACCOMMODATION_UID=a.accommodation_uid
   where u.ID=${userId}
-  limit 1;`;
+  limit 1;
+  `;
 
-  var sql2 = `select FNAME,LNAME,PHOTOURL,
-  count(COMPLETED_AT) as completed,
-  (count(CREATED_AT)-count(COMPLETED_AT)) as inProgress,
+  var sql2 = `
+  select FNAME,LNAME,PHOTOURL, count(COMPLETED_AT) as completed, 
+  (count(CREATED_AT)-count(COMPLETED_AT)) as inProgress, 
   sum(TIMESTAMPDIFF(DAY,CREATED_AT,COMPLETED_AT))/count(*) as days
   from ams_dashboard_users u join ams_dashboard_accommodations a on u.UID=a.USER_UID
   where u.ID=${userId}
-  group by(u.ID); `;
+  group by(u.ID);
+  `;
 
   connection.query(sql2, (error, rows1) => {
     connection.query(sql, (error, rows) => {
       if (error) response.send(error);
 
       let obj = {};
+
       for (let x = 0; x < rows.length; x++) {
+        var date = rows[x].CREATED_AT.toISOString();
+      
+        var YYYY = date.split("-")[0];
+        var MM = date.split("-")[1];
+        var D = date.split("-")[2];
+        var DD = D[0];
+        var DD = DD + D[1];
+        var HH = date.split("T")[1];
+        var HH = HH.split(".")[0];
+
         obj = {
           name: {
             name: rows1[x].FNAME,
@@ -106,7 +124,12 @@ module.exports.Details = (request, response) => {
             averageTime: rows1[x].days,
           },
           replies: {
-            lastReply: rows[x].UPDATED_AT,
+            lastReply: {
+              day: DD,
+              month: MM,
+              year: YYYY,
+              hour: HH,
+            },
             lastCompletedArea: {
               Area: rows[x].INQUIRY_ID,
               location: {
