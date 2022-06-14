5202db34efc7e23d898becf7816d11d6a7bacf6d
Jose Heron Samperio Leon <joseheronsamperio@gmail.com>
2022-06-14 09:02:59 -0500

    F por emmo

--- controllers/userController.js
+++ controllers/userController.js
@@ -250,9 +250,8 @@ module.exports.Details = (request, response) => {
 
 //COUNTRIES WHERE WTW HAS PRESENCE
 module.exports.Countries = (request, response) => {
-  var sql = ` SELECT distinct city FROM dashboard.ams_dashboard_accommodations order by city`;
-  var sql2 = `
-  SELECT distinct country_name FROM dashboard.ams_dashboard_accommodations order by country_name`;
+  var sql = ` SELECT distinct CITY FROM dashboard.ams_dashboard_accommodations order by city`;
+  var sql2 = `SELECT distinct country_name as COUNTRY FROM dashboard.ams_dashboard_accommodations order by country_name`;
   try {
     connection.query(sql2, (error, rows1) => {
       connection.query(sql, (error, rows) => {
